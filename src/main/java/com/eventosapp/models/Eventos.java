3d26b4c47ac364698ee2e966359edd94f4477936
ccaiomacedo <caio.macedo@ucsal.edu.br>
2021-05-02 23:34:08 -0300

    Creating database Connection

--- src/main/java/com/eventosapp/models/Eventos.java
+++ src/main/java/com/eventosapp/models/Eventos.java
@@ -1,10 +1,31 @@
 package com.eventosapp.models;
 
-public class Eventos {
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Eventos implements Serializable {
+	private static final long serialVersionUID = 1L;
+
 	private String nome;
 	private String data;
 	private String horario;
 	private String local;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long codigo;
+
+	public Long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(Long codigo) {
+		this.codigo = codigo;
+	}
 
 	public String getNome() {
 		return nome;
