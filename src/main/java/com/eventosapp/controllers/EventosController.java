4ab293bf95b1221d7281bbb69b02fd8ca7ab33ed
ccaiomacedo <caio.macedo@ucsal.edu.br>
2021-05-03 23:37:54 -0300

    implementing class that fetches data from the database

--- src/main/java/com/eventosapp/controllers/EventosController.java
+++ src/main/java/com/eventosapp/controllers/EventosController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.eventosapp.models.Eventos;
 import com.eventosapp.repository.EventoRepository;
@@ -23,6 +24,14 @@ public class EventosController {
 	public String form(Eventos evento) {
 		er.save(evento); //salva o evento no banco de dados
 		return "redirect:/cadastrarEvento";//redireciona pra cadastrarEvento
+	}
+	@RequestMapping("/eventos")
+	public ModelAndView listaEventos() {
+		ModelAndView mv= new ModelAndView("index");//estou especificando qual classe será renderizada
+		Iterable<Eventos> eventos = er.findAll();//retorna uma lista
+		mv.addObject("Eventos",eventos);
+		return mv;
+		
 	}
 	
 }
