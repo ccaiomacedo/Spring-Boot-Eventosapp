305ada320102e5edb8abbd5319d2fa7fac7f7883
ccaiomacedo <caio.macedo@ucsal.edu.br>
2021-08-13 23:39:00 -0300

    teste

--- src/main/java/com/eventosapp/controllers/IndexController.java
+++ src/main/java/com/eventosapp/controllers/IndexController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.eventosapp.models.Usuario;
-
+//teste
 @Controller
 public class IndexController {
 	@RequestMapping("/")
